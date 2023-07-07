1117907bed9a5891e750e8bf7ea2eed30eef8472
Prakash Timilsina <prakashtimilsina84@gmail.com>
2023-07-06 23:08:55 -0500

    login route, token is set on cookies.

--- src/app/api/users/login/route.tsx
+++ src/app/api/users/login/route.tsx
@@ -2,7 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { hasSubscribers } from "diagnostics_channel";
+import jwt from "jsonwebtoken";
 
 connect();
 
@@ -11,13 +11,39 @@ export async function POST(request: NextRequest) {
     const reqBody = await request.json();
     const { email, password } = reqBody;
     console.log(reqBody);
-
-    const response = NextResponse.json({ 
-        message: "Login sucessful", 
-        success: true
+    //Check if user exits?
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "user does not exist." },
+        { status: 400 }
+      );
+    }
+    //Check if password is correct
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json(
+        { error: "Invalid email/password" },
+        { status: 400 }
+      );
+    }
+    //Create Token Schema Data
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+    //Creating token
+    const token = await jwt.sign(tokenData, process.env.JWT_TOKEN_SECRET!, {
+      expiresIn: "1h",
     });
-    return response;
 
+    const response = NextResponse.json({
+      message: "Login sucessful",
+      success: true,
+    });
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
