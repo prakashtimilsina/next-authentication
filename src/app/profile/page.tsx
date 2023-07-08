40dd6fc9dbc931400292a538774982a9727a3bc6
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-07-07 23:03:15 -0500

    logout api added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,30 @@
+'use client'
 import React from 'react'
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
 
 const page = () => {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+     await axios.get('/api/users/logout')
+     router.push('/login')
+    } catch (error:any) {
+      console.log(error.message)
+    }
+  }
+  
+
   return (
-    <div>Profile Page</div>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1>Profile Page</h1>
+      <hr/>
+      <p>Profile details </p>
+      <hr />
+      <button onClick={logout} className='p-2 my-2 border border-gray-400 rounded-lg'>Sign Out</button>
+    </div>
   )
 }
 
